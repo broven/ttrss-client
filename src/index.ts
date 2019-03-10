91b0a079fcf964155684f695317e704060a712ae
metajs <zycode277@gmail.com>
2019-03-10 22:43:59 +0800

    chore: add pre-commit write tests

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,66 @@
+import axios from 'axios';
+
 interface Ioption {
     isTrustUnSecureCert?: boolean; // default set to true
-    serverUrl: number;
+    serverUrl: string;
+    timeout?: number;
 }
 
 
-
-class TtrssClient {
-    sessionId: string;
+const debug = (log: string) => {console.log(log); };
+/**
+ * Tiny tiny rss client class
+ */
+export default class TtrssClient {
+    private sessionId: string;
     apiLevel: number;
-    serverUrl: number;
-    isTrustUnSecureCert: boolean;
-    constructor(option: Ioption ) {
-
+    serverUrl: string;
+    timeout: number;
+    private isTrustUnSecureCert: boolean;
+    private userInfo: {user: string, password: string} = {user: "", password: ""};
+    constructor({
+        serverUrl,
+        timeout = 2000,
+        isTrustUnSecureCert = true,
+    }: Ioption ) {
+        debug(`init the class`);
+        this.serverUrl = serverUrl;
+        this.timeout = timeout;
+        this.isTrustUnSecureCert = isTrustUnSecureCert;
     }
-    public login() {
-
+    public async login(username: string, password: string) {
+        debug(`login...`);
+        this.userInfo.user = username;
+        this.userInfo.password = password;
+        return await this._login();
+    }
+    private async _login() {
+        const result = await this.sendRequest({
+            user: this.userInfo.user,
+            password: this.userInfo,
+        }, false);
+        debug(JSON.stringify(result));
+        return result;
+    }
+    public getVersion() {
+    }
+    private async sendRequest(operation: string, data: object, needLogin = true): Promise<object> {
+        // FIXME: '/' at the url end
+        if (needLogin) {
+            if (this.sessionId) {
+                Object.assign(data, {session_id: this.sessionId});
+            } else {
+                debug("in to the request login hell");
+                await this._login();
+                return await this.sendRequest(data, needLogin);
+            }
+        }
+        debug("senn request.");
+        try {
+            const res = await axios.post(this.serverUrl + "/api/", data, {timeout: this.timeout});
+        return res.data;
+        } catch (e) {
+            throw new Error(e);
+        }
     }
 }
---
+++ test/index.spec.ts
@@ -0,0 +1,24 @@
+import axios, { AxiosStatic } from 'axios';
+
+import TTRss from '../src/index';
+
+jest.mock("axios");
+
+describe("Base class", () => {
+test("default export should not be undefined", () => {
+    expect(TTRss).not.toBeUndefined();
+});
+});
+describe("login", () => {
+test("login", async () => {
+    (<jest.Mock>axios.post).mockImplementation((url, data, option) => {
+        expect(data).toContain({op: "login", user: "user", password: "admin"});
+        Promise.resolve({data: {"session_id": "xxx"}});
+    });
+    const ttrss = new TTRss({
+            serverUrl: "127.0.0.1"
+    });
+    const res = await ttrss.login("user", "admin");
+    expect(res).toEqual({"session_id": "xxx"});
+});
+});
